170155cc72ce46e8b2df42298f56d38e6b2108e5
Khyojae <khjae201@gmail.com>
2026-01-03 15:56:42 +0900

    chore: spring boot 초기 설정

---
+++ DOCKin-spring/src/main/java/com/DOCKin/DOCKin_spring/DocKinSpringApplication.java
@@ -0,0 +1,13 @@
+package com.DOCKin.DOCKin_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocKinSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocKinSpringApplication.class, args);
+	}
+
+}
---
+++ DOCKin-spring/src/test/java/com/DOCKin/DOCKin_spring/DocKinSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DOCKin.DOCKin_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocKinSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
