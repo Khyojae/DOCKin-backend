f938ea55b922810e712fb1fcf7d44dfced20ae03
Khyojae <khjae201@gmail.com>
2026-01-03 18:55:11 +0900

    feat: 에러 코드 정의 및 global 폴더 생성

---
+++ DOCKin-spring/src/main/java/com/DOCKin/controller/AttendanceController.java
@@ -0,0 +1,12 @@
+package com.DOCKin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/attendance")
+public class AttendanceController {
+
+}
