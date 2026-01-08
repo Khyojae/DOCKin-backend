9f008436f9c3d8ead03d211a992da2992768aa34
Khyojae <khjae201@gmail.com>
2026-01-08 16:30:39 +0900

    config: docker 설정

--- DOCKin-spring/src/main/resources/data.sql
+++ DOCKin-spring/src/main/resources/data.sql
@@ -6,3 +6,4 @@ VALUES ('1002', '이영희', '1234', 'ADMIN', '제8조선소', 'en', FALSE, NOW(
 INSERT INTO users (user_id, name, password, role, ship_yard_area, language_code, tts_enabled, created_at, updated_at)
 VALUES ('1003', '박민준', '1234', 'USER', '제5조선소', 'ko', TRUE, NOW(), NOW());
 SET FOREIGN_KEY_CHECKS = 1;
+
