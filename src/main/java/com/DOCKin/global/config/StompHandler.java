3b04b5da25c4ae5e88c6cc1e83059481bad44e3c
Khyojae <khjae201@gmail.com>
2026-01-16 00:45:38 +0900

    refactor: 중복 로그인 처리 로직 추가

--- src/main/java/com/DOCKin/global/config/StompHandler.java
+++ src/main/java/com/DOCKin/global/config/StompHandler.java
@@ -32,9 +32,11 @@ public class StompHandler implements ChannelInterceptor {
     public Message<?> preSend(Message<?> message, MessageChannel channel){
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 
-        if(StompCommand.CONNECT == accessor.getCommand()){
-            String token = accessor.getFirstNativeHeader("token");
 
+        // 1. 사용자가 처음 웹소켓 연결을 시도할 때 발생함
+        if(StompCommand.CONNECT == accessor.getCommand()){
+            //jwt 방식으로 입장
+            String token = accessor.getFirstNativeHeader("token");
             try{
                 //토큰이 없거나 유효하지 않으면 예외 발생
                 if(token == null || !jwtUtil.isValidToken(token)){
@@ -43,12 +45,17 @@ public class StompHandler implements ChannelInterceptor {
 
                 String userId = jwtUtil.getUserId(token);
 
+                //중복 로그인 처리 (같은 userId로 접속된 다른 sessionid가 있다면 제거)
+                onlineUsers.entrySet().removeIf(entry->entry.getValue().equals(userId));
+
+                //세션 방식 사용
                 if(accessor.getSessionAttributes()!=null){
                     accessor.getSessionAttributes().put("userId",userId);
                 }
 
                 //접속사 명단에 추가 (상태관리용)
                 onlineUsers.put(accessor.getSessionId(),userId);
+
                 log.info("유저 인증 성공: userId={}, 현재 접속자 수={}",userId,onlineUsers.size());
             } catch (Exception e){
                 log.error("웹소켓 인증 실패: {}",e.getMessage());
@@ -56,6 +63,7 @@ public class StompHandler implements ChannelInterceptor {
             }
         }
 
+        //2. 사용자가 특정 채팅방에 들어갈 때 발생함
         else if(StompCommand.SUBSCRIBE == accessor.getCommand()){
             String destination = accessor.getDestination();
             String userId = (String) accessor.getSessionAttributes().get("userId");
@@ -68,9 +76,16 @@ public class StompHandler implements ChannelInterceptor {
                 throw new MessageDeliveryException("해당 구역 채팅방에 접근 권한이 없습니다");
             }
         }
+
+        //연결 해제 시
+        else if(StompCommand.DISCONNECT == accessor.getCommand()){
+            String sessionId = accessor.getSessionId();
+            String userId = onlineUsers.remove(sessionId);
+        }
         return  message;
     }
 
+    //구역 번호를 추출
     public String extractZoneId(String destination){
         if(destination==null || !destination.contains("/sub/zone/")){
             return null;
