db2ab2c0e093229427e02e146833a8da4464fbe5
Khyojae <khjae201@gmail.com>
2026-01-18 17:24:32 +0900

    feat: ChatController 작성

---
+++ src/main/java/com/DOCKin/controller/ChatController.java
@@ -0,0 +1,27 @@
+package com.DOCKin.controller;
+
+import com.DOCKin.dto.chat.ChatMessageRequestDto;
+import com.DOCKin.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ChatController {
+    private final SimpMessagingTemplate messagingTemplate;
+    private final ChatService chatService;
+
+    @MessageMapping("/chat/message")
+    public void message(ChatMessageRequestDto message){
+        log.info("메시지 수신: 방번호={}, 보낸이={}, 내용={}",
+                message.getRoomId(),message.getSenderId(),message.getContent());
+
+        messagingTemplate.convertAndSend("/sub/chat/room/"+message.getRoomId());
+
+        chatService.saveMessage(message);
+    }
+}
