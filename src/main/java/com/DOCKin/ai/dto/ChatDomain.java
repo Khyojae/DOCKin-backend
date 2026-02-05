5952a9c0514281d3518ca5f2576b015d14de8c54
Khyojae <khjae201@gmail.com>
2026-02-06 03:18:46 +0900

    refactor: ai dto swagger 명시

--- src/main/java/com/DOCKin/ai/dto/ChatDomain.java
+++ src/main/java/com/DOCKin/ai/dto/ChatDomain.java
@@ -1,20 +1,42 @@
 package com.DOCKin.ai.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
+@Schema(description = "챗봇 dto")
 public class ChatDomain {
+    @Schema(description = "챗봇 request dto")
     public record Request(
+            @Schema(description = "메시지 항목")
             List<Message> messages,
+
+            @Schema(description = "언어")
             String lang,
+
+            @Schema(description = "추적 ID")
             String traceId
     ){
-        public record Message(String role, String content){}
+        @Schema(description = "개별 메시지 상세")
+        public record Message(
+                @Schema(description = "역할")
+                String role,
+
+                @Schema(description = "메시지 내용")
+                String content){}
     }
 
+    @Schema(description = "챗봇 response dto")
     public record Response(
+            @Schema(description = "추적 ID")
             String traceId,
+
+            @Schema(description = "결과 데이터")
             Result result
     ){
-        public record Result(String reply){}
+        @Schema(description = "응답 결과 상세")
+        public record Result(
+                @Schema(description = "챗봇 답변 내용")
+                String reply){}
     }
 }
--- src/main/java/com/DOCKin/ai/dto/TranslateDomain.java
+++ src/main/java/com/DOCKin/ai/dto/TranslateDomain.java
@@ -1,25 +1,49 @@
 package com.DOCKin.ai.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "번역 dto")
 public class TranslateDomain{
-    //작업일지 번역
+
+    @Schema(description = "작업일지 번역 request")
     public record Request(
+            @Schema(description = "원래 언어")
                              String source,
+
+                             @Schema(description = "바꿀 언어")
                              String target,
+
+                             @Schema(description = "추적ID")
                              String traceId){}
 
-    //실시간 번역 (원문이 있을때 사용)
+    @Schema(description = "실시간 번역 (원문이 있을때 사용) request")
     public record ApiRequest(
+
+            @Schema(description = "원래 문장")
             String text,
+
+            @Schema(description = "원래 언어")
             String source,
+
+            @Schema(description = "바꿀 언어")
             String target,
+
+            @Schema(description = "추적ID")
             String traceId
     ){}
 
-
-    //작업일지 응답 
+    @Schema(description = "작업일지 response")
     public record Response(
+
+            @Schema(description = "작업일지 제목")
             String title,
+
+            @Schema(description = "번역된 문장")
             String translated,
+
+            @Schema(description = "모델명")
             String model,
+
+            @Schema(description = "추적ID")
             String traceId){}
 }
